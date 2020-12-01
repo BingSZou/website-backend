80a4bfd5bfa2547c34568bf2d1866a366b614188
Susana Zou <susana_zou@ultimatesoftware.com>
2020-12-01 10:33:13 -0500

    Create deploy folder

---
+++ Deploy/database/init_script.sql
@@ -0,0 +1,42 @@
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+
+select 'I am testing this. 4444444  ***************.............';
+
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS test;
+
+select 'I am testing this.33  ....**************.........';
+USE test;
+
+CREATE TABLE IF NOT EXISTS Category (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS SubCategory (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	category_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS BookLibrary (
+	id INT NOT Null AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	author VARCHAR(255),
+	course_level INT,
+	link VARCHAR(600),
+	category_id INT,
+	subcategory_id INT,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO  Category (name)
+VALUES ('cat1'), ('cat2');
+INSERT INTO SubCategory (name, category_id)
+VALUES  ('sub1', 1), ('sub2', 2), ('sub3', 1), ('sub4', 1), ('sub5', 2);
